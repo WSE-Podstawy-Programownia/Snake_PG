c1a4b46698e8f6ca04dee3fd928addba35f6673b
Liafinelle <liafinelle@gmail.com>
2020-04-25 17:28:06 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,44 @@
+using System;
+using static System.Console;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+      string wiadomoscPowitalna = "\nWitaj w grze Snake! Naciśnij enter aby rozpocząć.";
+    WriteLine(wiadomoscPowitalna);
+    ReadLine();
+    Clear();
+
+    char[][] mapa = InicjujMape();
+    WypiszMape(mapa);
+  }
+
+  char[][] InicjujMape()
+  {
+    char[][] mapa = new char[50][];
+    for (int wymiar = 0; wymiar < 50; wymiar++)
+    {
+      mapa[wymiar] = new char[50];
+    }
+
+    for (int kolumna = 0; kolumna < 50; kolumna++)
+    {
+      for (int rzad = 0; rzad < 50; rzad++)
+      {
+        if (kolumna == 0 || kolumna == 49 || rzad == 0 || rzad == 49)
+        {
+          mapa[kolumna][rzad] = '#';
+        }
+        else {
+          mapa[kolumna][rzad] = ' ';
+        } 
+      }     
+    }
+
+    return mapa;
+  }
+
+  
+    }
+  }
+}
