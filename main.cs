32cb831573b8c81b0928acfd394a783f190fe8d8
liafinelle <liafinelle@gmail.com>
2020-05-30 16:57:11 +0000

    RandomizePoint

--- main.cs
+++ main.cs
@@ -1,50 +1,87 @@
 using System;
+using System.Threading;
 using static System.Console;
 
 class MainClass {
-  public static void Main (string[] args) 
+  static void WriteWelcomeMessage() 
   {
-      string wiadomoscPowitalna = "\nWitaj w grze Snake! Naciśnij enter aby rozpocząć.";
+    string wiadomoscPowitalna = "\nWitaj w grze Snake! Naciśnij enter aby rozpocząć.";
     WriteLine(wiadomoscPowitalna);
     ReadLine();
     Clear();
+  }
   
-    
-    char[][] mapa = new char[50][];
-    for (int wymiar = 0; wymiar < 50; wymiar++)
-    {
-      mapa[wymiar] = new char[50];
-    }
+  static int gameAreaXSize = 41;
+  static int gameAreaYSize = 21; 
+  static char [,] gameArea = new char[gameAreaXSize, gameAreaYSize];
+  static Point point = new Point();
+  static Snek snake = new Snek ();
 
-    for (int kolumna = 0; kolumna < 50; kolumna++)
-    {
-      for (int rzad = 0; rzad < 50; rzad++)
-      {
-        if (kolumna == 0 || kolumna == 49 || rzad == 0 || rzad == 49)
-        {
-          mapa[kolumna][rzad] = '#';
-        }      
-        else {
-          mapa[kolumna][rzad] = ' ';
-
-          
-        } 
-        Write(mapa[kolumna][rzad]);
-      } 
-      Write("\n");
-    }
-    
-    Random punkt = new Random();
-
-      for (int ctr = 1)
-    {
-   Console.Write("{0,6}", rnd.Next(-100, 101));
-   if (ctr % 5 == 0) Console.WriteLine();
-    }
-
+  static void PopulateGameArea () {
+    for (int y = 0; y < gameAreaYSize; y++) {
+        for (int x = 0; x < gameAreaXSize; x++) {
+          if(y == 0 || y == gameAreaYSize-1 || x == 0 || x == gameAreaXSize-1) {
+            gameArea[x, y] = '#'; 
+          }          
+          else {
+            gameArea[x, y] = ' '; // dokłądny zapis wymiarów mapy (żeby była ramka, a nie jakiś kwadrat głupi)
+          }
+        }
     }
   }
+
+  static void DrawGameArea () {
+    Console.Clear();
+    for (int y = 0; y < gameAreaYSize; y++) {
+        for (int x = 0; x < gameAreaXSize; x++) {
+          if (x == point.PointX && y == point.PointY) {
+            Console.Write('@');
+          }
+          
+          else {
+            Console.Write(gameArea[x, y]); // wypisanie kolejnego znaku w danej linijce
+          }
+        }
+        Console.WriteLine(""); // zeby zakonczyc linijke
+    }
+  }
+
+  public static void Main (string[] args) {
+    WriteWelcomeMessage(); // wypisanie wiadomosci powitalnej
+    PopulateGameArea(); // zapelnienie tablicy danymi, nie w while bo wystarczy, że wygeneruje się raz
+    
+    while (true){      // powtarzanie caly czas
+      point.RandomizePosition();
+      DrawGameArea(); // wypisanie obszaru gry
+      Thread.Sleep(2000); // odczekanie 500ms
+    }
+  }
+    }
+
+class Point {
+
+ public int PointX;
+ public int PointY;
+
+    public void RandomizePosition () {
+      
+      Random RandomizePoint = new Random();
+      PointX = RandomizePoint.Next(1,39);
+      PointY = RandomizePoint.Next(1,19);
+        
+  }
 }
+
+  class Snek {
+
+ public int SnekPointX;
+ public int SnekPointY;
+
+ 
+        
+  }
+
+
 
   
   
